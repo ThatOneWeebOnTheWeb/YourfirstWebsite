2a58dac89591879587cc23540520162b862cdadd
ThatOneWeebOnTheWeb <76894539+ThatOneWeebOnTheWeb@users.noreply.github.com>
2021-01-03 13:25:12 +0800

    Add files via upload

---
+++ Among Us.php
@@ -0,0 +1,433 @@
+<htmL>
+	<head><h1>AMONG US</h1>
+	<link rel="stylesheet" type="text/css" href="style.css"/>
+	
+	</head>
+	<bodY>
+	
+	<svg viewBox="0 0 500 500" style="border: green solid;">
+		
+		<path Stroke="green" stroke-width=".2" d="M10 0 V400"/>
+		<path Stroke="green" stroke-width=".2" d="M20 0 V400"/>
+		<path Stroke="green" stroke-width=".2" d="M30 0 V400"/>
+		<path Stroke="green" stroke-width=".2" d="M40 0 V400"/>
+		<path Stroke="green" stroke-width=".2" d="M50 0 V400"/>
+		<path Stroke="green" stroke-width=".2" d="M60 0 V400"/>
+		<path Stroke="green" stroke-width=".2" d="M70 0 V400"/>
+		<path Stroke="green" stroke-width=".2" d="M80 0 V400"/>
+		<path Stroke="green" stroke-width=".2" d="M90 0 V400"/>
+		<path Stroke="green" stroke-width=".2" d="M100 0 V400"/>
+		<path Stroke="green" stroke-width=".2" d="M110 0 V400"/>
+		<path Stroke="green" stroke-width=".2" d="M120 0 V400"/>
+		<path Stroke="green" stroke-width=".2" d="M130 0 V400"/>
+		<path Stroke="green" stroke-width=".2" d="M140 0 V400"/>
+		<path Stroke="green" stroke-width=".2" d="M150 0 V400"/>
+		<path Stroke="green" stroke-width=".2" d="M160 0 V400"/>
+		<path Stroke="green" stroke-width=".2" d="M170 0 V400"/>
+		<path Stroke="green" stroke-width=".2" d="M180 0 V400"/>
+		<path Stroke="green" stroke-width=".2" d="M190 0 V400"/>
+		<path Stroke="green" stroke-width=".2" d="M200 0 V400"/>
+		<path Stroke="green" stroke-width=".2" d="M210 0 V400"/>
+		<path Stroke="green" stroke-width=".2" d="M220 0 V400"/>
+		<path Stroke="green" stroke-width=".2" d="M230 0 V400"/>
+		<path Stroke="green" stroke-width=".2" d="M240 0 V400"/>
+		<path Stroke="green" stroke-width=".2" d="M250 0 V400"/>
+		<path Stroke="green" stroke-width=".2" d="M260 0 V400"/>
+		<path Stroke="green" stroke-width=".2" d="M270 0 V400"/>
+		<path Stroke="green" stroke-width=".2" d="M280 0 V400"/>
+		<path Stroke="green" stroke-width=".2" d="M290 0 V400"/>
+		
+		
+		
+		<path Stroke="green" stroke-width=".2" d="M0 10 H300"/>
+		<path Stroke="green" stroke-width=".2" d="M0 20 H300"/>
+		<path Stroke="green" stroke-width=".2" d="M0 30 H300"/>
+		<path Stroke="green" stroke-width=".2" d="M0 40 H300"/>
+		<path Stroke="green" stroke-width=".2" d="M0 50 H300"/>
+		<path Stroke="green" stroke-width=".2" d="M0 60 H300"/>
+		<path Stroke="green" stroke-width=".2" d="M0 70 H300"/>
+		<path Stroke="green" stroke-width=".2" d="M0 80 H300"/>
+		<path Stroke="green" stroke-width=".2" d="M0 90 H300"/>
+		<path Stroke="green" stroke-width=".2" d="M0 100 H300"/>
+		<path Stroke="green" stroke-width=".2" d="M0 110 H300"/>
+		<path Stroke="green" stroke-width=".2" d="M0 120 H300"/>
+		<path Stroke="green" stroke-width=".2" d="M0 130 H300"/>
+		<path Stroke="green" stroke-width=".2" d="M0 140 H300"/>
+		<path Stroke="green" stroke-width=".2" d="M0 150 H300"/>
+		<path Stroke="green" stroke-width=".2" d="M0 160 H300"/>
+		<path Stroke="green" stroke-width=".2" d="M0 170 H300"/>
+		<path Stroke="green" stroke-width=".2" d="M0 180 H300"/>
+		<path Stroke="green" stroke-width=".2" d="M0 190 H300"/>
+		<path Stroke="green" stroke-width=".2" d="M0 200 H300"/>
+		<path Stroke="green" stroke-width=".2" d="M0 210 H300"/>
+		<path Stroke="green" stroke-width=".2" d="M0 220 H300"/>
+		<path Stroke="green" stroke-width=".2" d="M0 230 H300"/>
+		<path Stroke="green" stroke-width=".2" d="M0 240 H300"/>
+		<path Stroke="green" stroke-width=".2" d="M0 250 H300"/>
+		<path Stroke="green" stroke-width=".2" d="M0 260 H300"/>
+		<path Stroke="green" stroke-width=".2" d="M0 270 H300"/>
+		<path Stroke="green" stroke-width=".2" d="M0 280 H300"/>
+		<path Stroke="green" stroke-width=".2" d="M0 290 H300"/>
+		<path Stroke="green" stroke-width=".2" d="M0 300 H300"/>
+		<path Stroke="green" stroke-width=".2" d="M0 310 H300"/>
+		<path Stroke="green" stroke-width=".2" d="M0 320 H300"/>
+		<path Stroke="green" stroke-width=".2" d="M0 330 H300"/>
+		<path Stroke="green" stroke-width=".2" d="M0 340 H300"/>
+		<path Stroke="green" stroke-width=".2" d="M0 350 H300"/>
+		<path Stroke="green" stroke-width=".2" d="M0 360 H300"/>
+		<path Stroke="green" stroke-width=".2" d="M0 370 H300"/>
+		<path Stroke="green" stroke-width=".2" d="M0 380 H300"/>
+		<path Stroke="green" stroke-width=".2" d="M0 390 H300"/>
+		<path Stroke="green" stroke-width=".2" d="M0 400 H300"/>
+		
+	
+	
+ <path id="curve" d="M110 60 Q80 110, 125,350 v120"/>
+ <text x="10" y="100" font-size="7px"style="fill:red;">
+
+    <textPath xlink:href="#curve">
+      "U Kinda Sus Bro"."I was at Cafeteria doing tasks mate". 
+    </textPath>
+	 </text>
+	 
+	 <path id="curve1" d="M110 230 H160 V110 "/>
+ <text x="0" y="100" font-size="7px"style="fill:red;">
+
+    <textPath xlink:href="#curve1">
+      "Im voting black he was acting sus". 
+    </textPath>
+	 </text>
+	  
+	 <path id="curve2" d="M157 180 H250 "/>
+ <text x="0" y="100" font-size="7px"style="fill:red;">
+
+    <textPath xlink:href="#curve2">
+      "Skipped Vote". 
+    </textPath>
+	
+	 </text>
+	   
+	 <path id="curve3" d="M200 180 H200 V270  "/>
+ <text x="0" y="100" font-size="7px"style="fill:red;">
+
+    <textPath xlink:href="#curve3">
+      "EMEGENCY 
+    </textPath>
+	
+	 </text>
+	
+	  <path id="curve4" d="M200 230 H250 V270  "/>
+ <text x="0" y="100" font-size="7px"style="fill:red;">
+
+    <textPath xlink:href="#curve4">
+      MEETING!".
+    </textPath>
+	</text>
+	
+	  <path id="curve5" d="M245 230 H245 V30  "/>
+ <text x="0" y="100" font-size="7px"style="fill:red;">
+
+    <textPath xlink:href="#curve5">
+      "O Imposter of The Vent what is your Wisdom?".
+    </textPath>
+	 </text>
+	 	  <path id="curve6" d="M110 70 Q160 5, 270,70 H245 V60  "/>
+ <text x="0" y="100" font-size="7px"style="fill:red;">
+
+    <textPath xlink:href="#curve6">
+      "Im stuck in vent please help".
+    </textPath>
+	</text>
+   <path id="curve7" d="M160 55  H245 V30 "/>
+ <text x="0" y="100" font-size="7px"style="fill:black;">
+
+    <textPath xlink:href="#curve7">
+      "Imma Just Skip Vote guys".
+    </textPath>
+	</text>
+	   <path id="curve8" d="M240 57  Q300 50, 130,270  H240 v150  "/>
+ <text x="0" y="100" font-size="7px"style="fill:black;">
+
+    <textPath xlink:href="#curve8">
+      "Black Vented".
+    </textPath>
+	</text>
+	 <path id="curve9" d="M155 90  H270  "/>
+ <text x="0" y="100" font-size="7px"style="fill:black;">
+
+    <textPath xlink:href="#curve9">
+      "Someone Watch Me do Medbay".
+    </textPath>
+	</text>
+	   <path id="curve10" d="M160 90  Q110 50, 300,20  H160 v150  "/>
+ <text x="0" y="100" font-size="7px"style="fill:black;">
+
+    <textPath xlink:href="#curve10">
+      "I saw him Vented".
+    </textPath>
+	</text>
+	
+	<path id="curve11" d="M105 220 H250 "/>
+ <text x="0" y="100" font-size="7px"style="fill:red;">
+
+    <textPath xlink:href="#curve11">
+      "Crewmate Tasks". 
+    </textPath>
+	
+	 </text>
+	 
+	<path id="curve12" d="M105 210 H250 "/>
+ <text x="0" y="100" font-size="7px"style="fill:red;">
+
+    <textPath xlink:href="#curve12">
+      "Fake Crew Tasks". 
+    </textPath>
+	
+	 </text>
+	 	<path id="curve13" d="M105 170 H250 "/>
+ <text x="0" y="100" font-size="6.5px"style="fill:red;">
+
+    <textPath xlink:href="#curve13">
+      "Fix Lights in Electrical"."Found a body at medbay". 
+    </textPath>
+	
+	 </text>
+	 	<path id="curve14" d="M105 200 H250 "/>
+ <text x="0" y="100" font-size="7px"style="fill:red;">
+
+    <textPath xlink:href="#curve14">
+      "Mini Crewmates". 
+    </textPath>
+	
+	 </text>
+	 <path id="curve15" d="M105 190 H250 "/>
+	 <text x="0" y="100" font-size="7px"style="fill:red;">
+
+    <textPath xlink:href="#curve15">
+      "Sabotage Lights". 
+    </textPath>
+	
+	 </text>
+	 <path id="curve16" d="M105 180 H250 "/>
+	 <text x="0" y="100" font-size="7px"style="fill:red;">
+
+    <textPath xlink:href="#curve16">
+      "I saw Green Vented!". 
+    </textPath>
+	
+	 </text>
+	  <path id="curve17" d="M100 140 H250 "/>
+	 <text x="0" y="100" font-size="16px"style="fill:red;">
+
+    <textPath xlink:href="#curve17">
+      "IMPOSTERS WIN!". 
+    </textPath>
+	
+	 </text>
+	  <path id="curve18" d="M205 220 H250 "/>
+	 <text x="0" y="100" font-size="7px"style="fill:red;">
+
+    <textPath xlink:href="#curve18">
+      "Red Sus". 
+    </textPath>
+	
+	 </text>
+	  <path id="curve19" d="M205 210 H250 "/>
+	 <text x="0" y="100" font-size="6.5px"style="fill:red;">
+
+    <textPath xlink:href="#curve19">
+      "Double Kill?". 
+    </textPath>
+	
+	 </text>
+	  <path id="curve20" d="M205 200 H250 "/>
+	 <text x="0" y="100" font-size="10px"style="fill:red;">
+
+    <textPath xlink:href="#curve20">
+      "Where?". 
+    </textPath>
+	
+	 </text>
+	   <path id="curve21" d="M205 190 H250 "/>
+	 <text x="0" y="100" font-size="8px"style="fill:red;">
+
+    <textPath xlink:href="#curve21">
+      "I did trash". 
+    </textPath>
+	
+	 </text>
+	    <path id="curve22" d="M200 180 H250 "/>
+	 <text x="0" y="100" font-size="8px"style="fill:red;">
+
+    <textPath xlink:href="#curve22">
+      "Simon Says". 
+    </textPath>
+	
+	 </text>
+	     <path id="curve23" d="M100 150 H250 "/>
+	 <text x="0" y="100" font-size="8px" font-weight="bold"style="fill:red;">
+
+    <textPath xlink:href="#curve23">
+      "OH COME ON! I was Doing Simon Says". 
+    </textPath>
+	
+	 </text>
+	 <path id="curve24" d="M100 160 H250 "/>
+	 <text x="0" y="100" font-size="8px"style="fill:red;">
+
+    <textPath xlink:href="#curve24">
+      "Gives Reason as why he is The Imposter". 
+    </textPath>
+	
+	 </text>
+	 <path id="curve25" d="M100 130 H250 "/>
+	 <text x="0" y="100" font-size="8px"style="fill:red;">
+
+    <textPath xlink:href="#curve25">
+      "Imposter First Game 0w0, Everyone Leaves". 
+    </textPath>
+	
+	 </text>
+	  <path id="curve26" d="M100 120 H250 "/>
+	 <text x="0" y="100" font-size="10px" font-weight="bold"style="fill:red;">
+
+    <textPath xlink:href="#curve26">
+      "Master Liar". 
+    </textPath>
+	
+	 </text>
+	 <path id="curve27" d="M160 120 H250 "/>
+	 <text x="0" y="100" font-size="10px" font-weight="bold"style="fill:red;">
+
+    <textPath xlink:href="#curve27">
+      "Master Detective". 
+    </textPath>
+	
+	 </text>
+	 <path id="curve28" d="M100 110 H250 "/>
+	 <text x="0" y="100" font-size="8px" style="fill:red;">
+
+    <textPath xlink:href="#curve28">
+      "Guys Wear Party Hats its my Birthday! :D". 
+    </textPath>
+	
+	 </text>
+	 <path id="curve29" d="M100 100 H250 "/>
+	 <text x="0" y="100" font-size="8px" style="fill:red;">
+
+    <textPath xlink:href="#curve29">
+      "Can Someone Fix lights Pls Im scared!". 
+    </textPath>
+	
+	 </text>
+	  <path id="curve30" d="M100 90 H250 "/>
+	 <text x="0" y="100" font-size="10px" font-weight="bold" style="fill:red;">
+
+    <textPath xlink:href="#curve30">
+      "Oxygen". 
+    </textPath>
+	
+	 </text>
+	  <path id="curve31" d="M105 80 H250 "/>
+	 <text x="0" y="100" font-size="7px" font-weight="bold" style="fill:red;">
+
+    <textPath xlink:href="#curve31">
+      "Key Tasks". 
+    </textPath>
+	
+	 </text>
+	   <path id="curve32" d="M115 70 H250 "/>
+	 <text x="0" y="100" font-size="7px" font-weight="bold" style="fill:red;">
+
+    <textPath xlink:href="#curve32">
+      "Reactor". 
+    </textPath>
+	
+	 </text>
+	   <path id="curve33" d="M120 60 H250 "/>
+	 <text x="0" y="100" font-size="7px" font-weight="bold" style="fill:red;">
+
+    <textPath xlink:href="#curve33">
+      "Ghosts". 
+    </textPath>
+	
+	 </text>
+	    <path id="curve34" d="M140 50 H250 "/>
+	 <text x="0" y="100" font-size="7px" font-weight="bold" style="fill:red;">
+
+    <textPath xlink:href="#curve34">
+      "Ser Voltaire Was Not The Imposter". 
+    </textPath>
+	
+	 </text>
+	    <path id="curve35" d="M160 45 H250 "/>
+	 <text x="0" y="100" font-size="7px" font-weight="bold" style="fill:red;">
+
+    <textPath xlink:href="#curve35">
+      "1 Imposter Remaining". 
+    </textPath>
+	
+	 </text>
+	 <path id="curve36" d="M180 40 H250 "/>
+	 <text x="0" y="100" font-size="7px" font-weight="bold" style="fill:red;">
+
+    <textPath xlink:href="#curve36">
+      "Kill Cooldown". 
+    </textPath>
+	
+	 </text>
+	  <path id="curve37" d="M160 60 H250 "/>
+	 <text x="0" y="100" font-size="7px" font-weight="bold" style="fill:White;">
+
+    <textPath xlink:href="#curve37">
+      "CARD SWIPE TASK". 
+    </textPath>
+	
+	 </text>
+	  <path id="curve38" d="M160 70 H270 "/>
+	 <text x="0" y="100" font-size="7px" font-weight="bold" style="fill:White;">
+
+    <textPath xlink:href="#curve38">
+      "Win By Sabotaging Oxygen". 
+    </textPath>
+	
+	 </text>
+	   <path id="curve39" d="M160 80 H270 "/>
+	 <text x="0" y="100" font-size="6px" font-weight="bold" style="fill:White;">
+
+    <textPath xlink:href="#curve39">
+      "JUDGEMENT CALL AND INNO". 
+    </textPath>
+	
+	 </text>
+	 <path id="curve40" d="M170 90  Q110 50, 300,20  H160 v150  "/>
+ <text x="0" y="100" font-size="7px"style="fill:white;">
+
+    <textPath xlink:href="#curve40">
+      "Vote Cheater".
+    </textPath>
+	</text>
+	 <path id="curve41" d="M100 80  H80 V200  "/>
+ <text x="0" y="100" font-size="7px"style="fill:red;">
+
+    <textPath xlink:href="#curve41">
+      "Vote the Cheater before Voting the Imposter".
+    </textPath>
+	</text>
+	<path id="curve42" d="M105 85  H85 V200  "/>
+ <text x="0" y="100" font-size="7px"style="fill:red;">
+
+    <textPath xlink:href="#curve42">
+      "Guys I think Blue did A Self Report there".
+    </textPath>
+	</text>
+		<path id="curve43" d="M90 90   V200  "/>
+ <text x="0" y="100" font-size="10px"style="fill:red;">
+
+    <textPath xlink:href="#curve43">
+      "I saw RED do Medbay".
+    </textPath>
+	</text>
+	</svg>
+	</body>
+	</html>
